57059ac46a6a4c495c87ace766d8da913a0d0d33
Furkan Ulutaş <furkanulutas@icloud.com>
2023-12-09 00:13:20 +0300

    feat: added responsivity

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -10,21 +10,21 @@ const handleSubmit = async (e) => {
 
 export default function Home() {
   return (
-    <div className="h-screen flex justify-center items-center">
-      <div className="h-[75%] w-[55%] bg-gray-200 rounded-3xl flex justify-center p-6">
-        <div className="h-full w-[30rem] flex justify-center items-center flex-col">
+    <div className="h-screen flex justify-center items-center p-3 ">
+      <div className="max-w-sm p-5 bg-gray-200 rounded-3xl sm:max-w-6xl sm:p-12">
+        <div className="flex justify-center items-center flex-col">
           <img src="./public/loopLogo.png" alt="loopLogo" className="w-48 my-2" />
           <p className="text-center font-bold my-2">Başvuru Sorgulama Ekranı</p>
           <div className="bg-gradient-to-r from-blue-900 to-purple-500 h-1 w-[75%] rounded-3xl mb-2 " />
           <div className="alertbox flex justify-center gap-2">
             <img src="./public/notification-bell.png" alt="" className="w-12" />
-            <div className="w-[58%] h-full bg-zinc-300 rounded-full px-3">
+            <div className="max-w-sm h-full bg-zinc-300 rounded-full">
               <p className="text-xs text-center m-2 ">
                 Çekirdek(Core) Takım başvurusu yaparken kullandığınız bilgileri kullanınız!
               </p>
             </div>
           </div>
-          <form onSubmit={handleSubmit} className="flex flex-col my-2 w-[70%]">
+          <form onSubmit={handleSubmit} className="flex flex-col my-2 max-w-sm sm:max-w-6xl">
             <p className="text-sm font-bold mt-2">Mail Adresiniz:</p>
             <input
               type="text"
